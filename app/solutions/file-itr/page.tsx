8fb9c16d39c1ddf4a704a955945fb8ff45630001
swati0dev <choudharyswati2907@gmail.com>
2026-08-01 09:45:42 +0530

    fix: resolve import error for ITRWizard in file-itr page

--- app/solutions/file-itr/page.tsx
+++ app/solutions/file-itr/page.tsx
@@ -1,4 +1,4 @@
-import { ITRWizard } from "@/components/tools/ITRWizard";
+import ITRWizard from "@/components/tools/ITRWizard";
 import { FileCheck2 } from "lucide-react";
 
 export default function FileITRPage() {
